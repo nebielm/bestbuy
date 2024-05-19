2d22f81feb437aaaccdfde14242590a497e214f1
Nebiel Mohammed <nebielmohammedhotmail.com@Air-von-Nebiel.fritz.box>
2024-05-19 13:52:45 +0200

    create store class

---
+++ store.py
@@ -0,0 +1,31 @@
+class Store:
+    def __init__(self, product_list):
+        self.product_list = product_list
+
+    def add_product(self, product):
+        if product in self.product_list:
+            raise Exception("Product already in store.")
+        self.product_list.append(product)
+
+    def remove_product(self, product):
+        if product not in self.product_list:
+            raise Exception("Product not in store.")
+        self.product_list.remove(product)
+
+    def get_total_quantity(self):
+        return len(self.product_list)
+
+    def get_all_products(self):
+        active_products = []
+        for product in self.product_list:
+            if product.active:
+                active_products.append(product)
+        return active_products
+
+    def order(self, shopping_list):
+        total_price = 0
+        for item in shopping_list:
+            product = item[0]
+            quantity = item[1]
+            total_price += product.buy(quantity)
+        return total_price
